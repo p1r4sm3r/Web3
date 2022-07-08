29eda0de0668eb7a1f9cf94fb89c01009aed298b
t1les <78717377+t1les@users.noreply.github.com>
2022-07-08 17:16:42 +0700

    Add files via upload

---
+++ NFT COMPOSITOR/generate_avatar.py
@@ -0,0 +1,9 @@
+from avatar_generator import AvatarGenerator
+
+
+def generate_avatar():
+    generator = AvatarGenerator("./images")
+    generator.generate_avatar(100)
+
+if __name__=="__main__":
+    generate_avatar()
